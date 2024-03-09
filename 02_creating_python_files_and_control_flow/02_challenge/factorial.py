c91754192a7a8605c95c3a99a3dad94875ab1e7d
momoko <momoko.yamada@woven.toyota>
2024-03-09 09:29:29 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,10 +1,16 @@
 def compute_factorial():
     # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = 0
+    number = int(input("Please input number:"))
+    if (number < 0):
+        number = number * (-1)
 
     # result変数を編集し、最終的な計算結果を保存します
-    result = 0
+    result = 1
+    while (number >= 1):
+        result = result * number
+        number = number - 1
+    print(result)
 
     return result
 
