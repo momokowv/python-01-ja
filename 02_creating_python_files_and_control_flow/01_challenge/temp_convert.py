9d823c5741e39643895cc5991942d8d9a06a9bdd
momoko <momoko.yamada@woven.toyota>
2024-03-09 09:00:05 +0900

    Submit challenge for granding

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,7 +1,23 @@
 def convert():
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    temp=int(input("Please input temperature:"))
+    unit=input("Please input unit:")
+    unit_f="f"
+    unit_c="c"
+    if(unit==unit_f):
+        unit="c"
+        temp= (temp-32)*5/9
+        temp=int(temp)
+        print(str(temp) + unit)
+    elif (unit==unit_c):
+        unit="f"
+        temp= temp*9/5+32
+        temp=int(temp)
+        print(str(temp) + unit)
+    else:
+        print("Error - Please input f or c for unit.")
+
 
     return temp
 
