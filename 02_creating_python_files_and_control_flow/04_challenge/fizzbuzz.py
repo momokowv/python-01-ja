6cdbadf05e53976fbd622955854ff67a46fb75d3
momoko <momoko.yamada@woven.toyota>
2024-03-09 16:32:25 +0900

    Submitting for granding

--- 02_creating_python_files_and_control_flow/04_challenge/fizzbuzz.py
+++ 02_creating_python_files_and_control_flow/04_challenge/fizzbuzz.py
@@ -1,1 +1,11 @@
 # ここにコードを書いてください
+for x in range(1,101):
+    if (x % 3 == 0 and x % 5 == 0):
+        x = "FizzBuzz"
+    elif (x % 3 ==0):
+        x = "Fizz"
+    elif (x % 5 ==0):
+        x = "Buzz"
+    else:
+        x = x
+    print(x)
