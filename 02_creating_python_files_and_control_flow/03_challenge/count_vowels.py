7389e1439162e6a5e39197a0edbb7f376119762a
momoko <momoko.yamada@woven.toyota>
2024-03-09 16:20:47 +0900

    Submitting for granding

--- 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
+++ 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
@@ -1,1 +1,9 @@
 # ここにコードを書いてください
+
+number_vowels = 0
+word = input ("Enter a string:")
+for x in word:
+    if( x == "a" or x == "i" or x == "u" or x == "e" or x == "o"):
+        number_vowels = number_vowels + 1
+
+print("Number of vowels:" + str(number_vowels))
