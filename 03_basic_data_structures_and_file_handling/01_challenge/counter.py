4d74d0a3906ecf6aaaf4d1ff1342c4407d5f41b5
momoko <momoko.yamada@woven.toyota>
2024-03-17 08:11:27 +0900

    Submit for granding

--- 03_basic_data_structures_and_file_handling/01_challenge/counter.py
+++ 03_basic_data_structures_and_file_handling/01_challenge/counter.py
@@ -13,7 +13,16 @@ def counter():
         "strawberry",
     ]
 
-    # ここにコードを書いてください
+    counts=[]
+    for x in fruits:
+        counts.append(fruits.count(str(x)))
+
+    occurrences=dict(zip(fruits,counts))
+
+    for k, v in occurrences.items():
+        print(k, ":", v)
+
+        
 
     return occurrences
 
