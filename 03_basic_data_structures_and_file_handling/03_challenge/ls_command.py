633d3bfcb078d024440f5106fc3a2016d8ede5ec
momoko <momoko.yamada@woven.toyota>
2024-03-18 09:38:53 +0900

    Submit for granding

--- 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
+++ 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 
 def ls_command(directory):
@@ -6,7 +7,17 @@ def ls_command(directory):
     # 無効なディレクトリパスについて考える必要はありません
 
     # 「pass」を削除して、ここにコードを書いてください
-    pass
+    
+    files = os.listdir(str(directory))
+    file_dict = defaultdict(list)
+
+    for x in files:
+         if os.path.isfile(os.path.join(directory, x)):
+             _, ext = os.path.splitext(x)
+             file_dict[ext].append(x)
+
+    for ext, file_list in sorted(file_dict.items()):
+        print(f"{ext}: {len(file_list)} file{'s' if len(file_list) != 1 else ''}")
 
 
 if __name__ == "__main__":
