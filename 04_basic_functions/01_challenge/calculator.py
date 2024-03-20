ce414c87a302f02f5e687ab5548394a471ed8a73
momoko <momoko.yamada@woven.toyota>
2024-03-21 00:04:14 +0900

    Submit for granding

--- 04_basic_functions/01_challenge/calculator.py
+++ 04_basic_functions/01_challenge/calculator.py
@@ -1,14 +1,53 @@
 def add(a, b):
-    return 0
+    if isinstance(a,str) or isinstance(b,str):
+        result = "bad input"
+        print(result)
+        return result
+    else:
+        result = a + b
+        print("The result of adding:",result)
+        return result
 
 
 def subtract(a, b):
-    return 0
+    if isinstance(a,str) or isinstance(b,str):
+        result = "bad input"
+        print(result)
+        return result
+    else:
+        result = a - b
+        print("The result of subtracting:",result)
+        return result
 
 
 def multiply(a, b):
-    return 0
+    if isinstance(a,str) or isinstance(b,str):
+        result = "bad input"
+        print(result)
+        return result
+    else:
+        result = a * b
+        print("The result of multiplying:",result)
+        return result
 
 
 def divide(a, b):
-    return 0
+    if isinstance(a,str) or isinstance(b,str):
+        result = "bad input"
+        print(result)
+        return result
+    elif a == 0 or b == 0:
+        result = "Cannot divide by zero"
+        print(result)
+        return result
+    else:
+        result = a / b
+        print("The result of dividing:",result)
+        return result
+
+
+add("Hello",3)
+subtract("Hello",3)
+multiply("Hello",3)
+divide(0,3)
+
