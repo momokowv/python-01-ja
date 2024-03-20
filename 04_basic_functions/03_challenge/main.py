f3582eee21a221ca9e69aa9101c4325b25f978b9
momoko <momoko.yamada@woven.toyota>
2024-03-21 01:14:22 +0900

    Submit for granding

--- 04_basic_functions/03_challenge/main.py
+++ 04_basic_functions/03_challenge/main.py
@@ -1,6 +1,16 @@
 def unique_substrings(input_string):
-    # 「pass」を削除して、ここにコードを書いてください
-    pass
+    
+    strings = []
+    y=0
+
+    for y in range(len(input_string)):
+        for x in range(y,len(input_string)):
+            if input_string[y:x+1] not in strings:
+                strings.append(input_string[y:x+1])
+    
+    
+    return strings
+        
 
 
 input_string = "banana"
